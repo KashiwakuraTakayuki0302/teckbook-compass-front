6468cda0600c13f9cf169babe5a99f1fb18d7655
kashiwakura <ginmedaldoumedal@gmail.com>
2025-12-13 14:15:23 +0900

    GoogleSearchConsoleの導入

--- client/src/hooks/usePageTracking.ts
+++ client/src/hooks/usePageTracking.ts
@@ -2,23 +2,23 @@ import { useEffect } from 'react';
 import { useLocation } from 'wouter';
 
 declare global {
-    interface Window {
-        gtag?: (
-            command: 'config' | 'event' | 'js',
-            targetId: string | Date,
-            config?: Record<string, any>
-        ) => void;
-    }
+  interface Window {
+    gtag?: (
+      command: 'config' | 'event' | 'js',
+      targetId: string | Date,
+      config?: Record<string, any>
+    ) => void;
+  }
 }
 
 export const usePageTracking = () => {
-    const [location] = useLocation();
+  const [location] = useLocation();
 
-    useEffect(() => {
-        if (typeof window.gtag !== 'undefined') {
-            window.gtag('config', 'G-Y29K7T366Z', {
-                page_path: location,
-            });
-        }
-    }, [location]);
+  useEffect(() => {
+    if (typeof window.gtag !== 'undefined') {
+      window.gtag('config', 'G-Y29K7T366Z', {
+        page_path: location,
+      });
+    }
+  }, [location]);
 };
